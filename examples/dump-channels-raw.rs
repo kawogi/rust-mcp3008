b706c3ecbd4faa3890c3ba2d87e923cd64ebc845
kai.giebeler <kai.giebeler@audiotool.com>
2020-05-19 18:43:38 +0200

    improve console output

--- examples/dump-channels-raw.rs
+++ examples/dump-channels-raw.rs
@@ -5,7 +5,7 @@ use mcp3208::Mcp3208;
 fn main() {
     if let Ok(mut mcp3208) = Mcp3208::new("/dev/spidev0.0") {
         for channel_id in 0..8 {
-            println!("{}", mcp3208.read_adc(channel_id).unwrap());
+            println!("channel #{}: {}", channel_id, mcp3208.read_adc(channel_id).unwrap());
         }
     }
 }
